ab3e0263b850b95c4c8ec9006a4226891ce9a597
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-04-28 01:25:05 -0700

    new plots, new images, spark session class

--- create_plots.py
+++ create_plots.py
@@ -1,25 +1,38 @@
-import pyspark
-from pyspark.sql import SparkSession
-
-# import plotly.plotly as py
 import plotly.graph_objs as go
 
-conf = pyspark.SparkConf().setAll([
-         ('spark.master',   'local[*]'),
-         ('spark.app.name', 'PySpark Demo')])
-spark = SparkSession.builder.config(conf=conf).getOrCreate()
+from spark_session import Create_Spark_Session
 
-df = spark.read.csv("flight-delays/flights.csv",
-                    header=True).cache()
 
-origin_airport_count = df.groupBy('ORIGIN_AIRPORT').count().orderBy('count', ascending=False)
+create_spark_session = Create_Spark_Session()
+df = create_spark_session.create_spark_df()
 
-fig = go.Figure([go.Bar(x=origin_airport_count.toPandas()['ORIGIN_AIRPORT'], y=origin_airport_count.toPandas()['count'])])
-fig.update_layout(title={'text': "Origin Airport Counts",
-                         'x':0.5,
-                         'xanchor': 'center',
-                         'yanchor': 'top'},
-                 xaxis_title="Origin Aiport",
-                 yaxis_title="Count")
-fig.write_image("EDA_Images/Origin_Airport_Count.png")
-fig.show()
+class Create_EDA_Plots:
+        def origin_airport_counts(self, df):
+                origin_airport_count = df.groupBy('ORIGIN_AIRPORT').count().orderBy('count', ascending=False)
+
+                fig = go.Figure([go.Bar(x = origin_airport_count.toPandas()['ORIGIN_AIRPORT'], 
+                                        y = origin_airport_count.toPandas()['count'])])
+                fig.update_layout(title={'text': "Origin Airport Counts",
+                                         'x':0.5,
+                                         'xanchor': 'center',
+                                         'yanchor': 'top'},
+                                  xaxis_title = "Origin Aiport",
+                                  yaxis_title = "Count")
+                fig.write_image("EDA_Images/Origin_Airport_Count.png")
+
+        def destination_airport_counts(self, df):
+                destination_airport_count = df.groupBy('DESTINATION_AIRPORT').count().orderBy('count', ascending=False)
+
+                fig = go.Figure([go.Bar(x = destination_airport_count.toPandas()['DESTINATION_AIRPORT'], 
+                                        y=destination_airport_count.toPandas()['count'])])
+                fig.update_layout(title={'text': "Destination Airport Counts",
+                                         'x':0.5,
+                                         'xanchor': 'center',
+                                         'yanchor': 'top'},
+                                  xaxis_title = "Destination Aiport",
+                                  yaxis_title = "Count")
+                fig.write_image("EDA_Images/Destination_Airport_Count.png")
+
+create_eda_plots = Create_EDA_Plots()
+create_eda_plots.origin_airport_counts(df = df)
+create_eda_plots.destination_airport_counts(df = df)
---
+++ spark_session.py
@@ -0,0 +1,53 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+class Create_Spark_Session:
+    def create_spark_df(self):
+        # Spark configuration
+        conf = pyspark.SparkConf().setAll([
+                ('spark.master',   'local[*]'),
+                ('spark.app.name', 'PySpark Demo')])
+
+        # Schema for original data set
+        schema = StructType([StructField("YEAR", StringType(), False),
+                             StructField("MONTH", StringType(), False),
+                             StructField("DAY", StringType(), False),
+                             StructField("DAY_OF_WEEK", StringType(), False),
+                             StructField("AIRLINE", StringType(), False),
+                             StructField("FLIGHT_NUMBER", StringType(), True),
+                             StructField("TAIL_NUMBER", StringType(), True),
+                             StructField("ORIGIN_AIRPORT", StringType(), False),
+                             StructField("DESTINATION_AIRPORT", StringType(), False),
+                             StructField("SCHEDULED_DEPARTURE", IntegerType(), False),
+                             StructField("DEPARTURE_TIME", IntegerType(), True),
+                             StructField("DEPARTURE_DELAY", IntegerType(), True),
+                             StructField("TAXI_OUT", IntegerType(), True),
+                             StructField("WHEELS_OFF", IntegerType(), True),
+                             StructField("SCHEDULED_TIME", IntegerType(), True),
+                             StructField("ELAPSED_TIME", IntegerType(), True),
+                             StructField("AIR_TIME", IntegerType(), True),
+                             StructField("DISTANCE", IntegerType(), True),
+                             StructField("WHEELS_ON", IntegerType(), True),
+                             StructField("TAXI_IN", IntegerType(), True),
+                             StructField("SCHEDULED_ARRIVAL", IntegerType(), True),
+                             StructField("ARRIVAL_TIME", IntegerType(), True),
+                             StructField("ARRIVAL_DELAY", IntegerType(), True),
+                             StructField("DIVERTED", StringType(), True),
+                             StructField("CANCELLED", StringType(), True),
+                             StructField("CANCELLATION_REASON", StringType(), True),
+                             StructField("AIR_SYSTEM_DELAY", IntegerType(), True),
+                             StructField("SECURITY_DELAY", IntegerType(), True),
+                             StructField("AIRLINE_DELAY", IntegerType(), True),
+                             StructField("LATE_AIRCRAFT_DELAY", IntegerType(), True),
+                             StructField("WEATHER_DELAY", StringType(), True),
+                            ])
+
+        # Creating Spark session
+        spark = SparkSession.builder.config(conf=conf).getOrCreate()
+        spark_df = spark.read.csv("flight-delays/flights.csv",
+                                  header=True,
+                                  schema = schema).cache()
+
+        # Return spark dataframe
+        return spark_df
