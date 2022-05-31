94604ec9dedb444bcdc88750892a9299aa8aba03
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-05-31 16:29:35 -0700

    removing this file, moved to main

--- download_data.py
+++
@@ -1,9 +0,0 @@
-import opendatasets as od
-
-class Download_Data:
-    def download_kaggle_data(self):
-        dataset_url = "https://www.kaggle.com/datasets/usdot/flight-delays?select=flights.csv"
-        od.download("{}".format(dataset_url))
-
-download_data = Download_Data()
-download_data.download_kaggle_data()
